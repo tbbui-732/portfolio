ef075afd3b16a1a335e8b71002cfbd38cb2c447e
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-02-07 15:59:29 -0500

    Create an outer ring animation for the cursor that expands when clicked

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-const cursor = document.querySelector('.cursor');
+const outerRing = document.querySelector('.outer-ring');
 
+// outer ring moves with mouse
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+    outerRing.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
 });
 
+// outer ring expands when clicked
 document.addEventListener('click', () => {
-    cursor.classList.add("expand");
+    outerRing.classList.add("expand");
     setTimeout(() => {
-        cursor.classList.remove("expand");
+        outerRing.classList.remove("expand");
     }, 500);
 });
+
