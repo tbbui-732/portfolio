f5173040702c41262695e7249cbf65cb076339bb
Bao <80117447+tbbui-732@users.noreply.github.com>
2025-02-17 12:11:00 -0500

    Cursor periodically pulsates and expands when clicked

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const cursorOuter = document.querySelector('.cursor-outer');
 let posX = 0, posY = 0; // Inner dot position
 let trailingX = 0, trailingY = 0; // Outer ring position
 
+// inner cursor follows mouse
 document.addEventListener("mousemove", (e) => {
     posX = e.clientX;
     posY = e.clientY;
@@ -12,6 +13,7 @@ document.addEventListener("mousemove", (e) => {
     cursorInner.style.top = `${posY}px`;
 });
 
+// outer cursor animation
 function animateOuterCursor() {
     trailingX += (posX - trailingX) * 0.1;
     trailingY += (posY - trailingY) * 0.1;
@@ -21,5 +23,12 @@ function animateOuterCursor() {
 
     requestAnimationFrame(animateOuterCursor);
 }
-
 animateOuterCursor();
+
+// outer cursor expands when clicked
+document.addEventListener("click", (e) => {
+    cursorOuter.classList.add("expand");
+    setTimeout(() => {
+        cursorOuter.classList.remove("expand");
+    }, 500);
+});
